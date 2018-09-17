a83446c117d210a68a6a793e80f0f26370e0980d
yedmit <softproto@gmail.com>
2018-09-17 14:49:54 +0300

    some new features

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q*h_@+2$7r4%y&s1foow4=dd1=7au5dw)vdaq-fse3-6__%tmm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'gippopotam.pythonanywhere.com', 'klunak.ru']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'gippopotam.pythonanywhere.com', 'klunak.ru']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'polls',
 ]
 
 MIDDLEWARE = [
