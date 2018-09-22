2b622881e51f28c6c3993e7df84c406cddf621b9
yedmit <softproto@gmail.com>
2018-09-22 22:11:53 +0300

    next step

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.post_list, name='post_list'),
+    path('index/', views.index, name='index'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the blog index.")
+
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
